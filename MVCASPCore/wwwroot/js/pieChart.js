894711466c3a3f90c2395008b8d895f0f2d8914b
kepatotorica <kepatoto@gmail.com>
2018-12-18 15:31:12 -0700

    lol gitignore was not working, got a basic react component to show up

---
+++ MVCASPCore/wwwroot/js/pieChart.js
@@ -0,0 +1,17 @@
+﻿//npx babel--watch. --out - dir wwwroot / dist--presets react - app / prod
+//from root 
+
+var e = React.createElement;
+
+class HelloWorld extends React.Component {
+    render() {
+        return e(
+            "div",
+            null,
+            "Hello World"
+        );
+    }
+}
+
+const containerElement = document.getElementById('chart');
+ReactDOM.render(e(HelloWorld), containerElement);
