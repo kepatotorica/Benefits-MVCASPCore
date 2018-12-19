41038352ad18f6cdd531d799b220594b67e4b576
kepatotorica <kepatoto@gmail.com>
2018-12-19 01:49:41 -0700

    FINALLY got json working

--- MVCASPCore/Scripts/Home/react/all.js
+++ MVCASPCore/Scripts/Home/react/all.js
@@ -1,6 +1,6 @@
 ﻿const { PieChart, Pie, Sector, Cell } = Recharts;
-const data = [{ name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
-{ name: 'Group C', value: 300 }, { name: 'Group D', value: 200 }];
+//const data = [{ name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
+//{ name: 'Group C', value: 300 }, { name: 'Group D', value: 200 }];
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 const RADIAN = Math.PI / 180;
 
