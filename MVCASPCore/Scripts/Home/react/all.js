e2ae17145e01976875c133c070e9b743a5149882
Kepa Totorica <kepatoto@gmail.com>
2018-12-19 18:13:20 -0700

    made one of the charts look better

--- MVCASPCore/Scripts/Home/react/all.js
+++ MVCASPCore/Scripts/Home/react/all.js
@@ -1,27 +1,59 @@
 ﻿//data is in Users/Details.cshtml
+const { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } = Recharts;
 
-const { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } = Recharts;
 var React = require('react');
-var SimpleLineChart = require('create-react-class');
-import { render } from 'react-dom';
 
-const SimpleLineChart = () => (
-    <LineChart width={600} height={400} data={data}
+const StackedBarChart = () =>
+(
+    <BarChart width={600} height={400} data={data}
         margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+        <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
-        <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
-);
+            <Bar dataKey="Paycheck" stackId="a" fill="#1d993e" />
+            <Bar dataKey="Deductions" stackId="a" fill="#991d1d" />
+            <Bar dataKey="Benefits" fill="#1d6d99" />
+    </BarChart>
+)
 
-render(
-    <SimpleLineChart />,
+ReactDOM.render(
+    <StackedBarChart />,
     document.getElementById('container')
 );
+
+
+
+
+
+
+
+
+
+
+//const { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } = Recharts;
+//var React = require('react');
+//var SimpleLineChart = require('create-react-class');
+//import { render } from 'react-dom';
+
+//const SimpleLineChart = () => (
+//    <LineChart width={600} height={400} data={data}
+//        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+//        <XAxis dataKey="name" />
+//        <YAxis />
+//        <CartesianGrid strokeDasharray="3 3" />
+//        <Tooltip />
+//        <Legend />
+//        <Line type="monotone" dataKey="paycheck" stroke="#8884d8" activeDot={{ r: 8 }} />
+//        <Line type="monotone" dataKey="benefits" stroke="#82ca9d" />
+//    </LineChart>
+//);
+
+//render(
+//    <SimpleLineChart />,
+//    document.getElementById('container')
+//);
 
 
 
